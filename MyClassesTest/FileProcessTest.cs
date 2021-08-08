9e2a1edc86049315d3aa5dcf07d4de2e41781bff
Paiva <danpaiva@live.com>
2021-08-08 19:42:18 -0300

    FileProcess Test

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+using System;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            FileProcess fileProcess = new FileProcess();
+            bool fromCall;
+
+            fromCall = fileProcess.FileExists(@"c:\Windows\Regedit.exe");
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            FileProcess fileProcess = new FileProcess();
+            bool fromCall;
+
+            fromCall = fileProcess.FileExists(@"c:Regedit.exe");
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            FileProcess fileProcess = new FileProcess();
+
+            fileProcess.FileExists("");
+        }
+    }
+}
---
+++ MyClassesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClassesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c298973-535a-4270-96e5-46ca9a26857e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
